c2891d2345de4d8871612d645162edfae70bd564
eprelofcdesktop <eprelofcdesktop@am.com>
2019-05-09 08:47:59 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send(process.env));
+app.listen(port, () => console.log('Server is running on port ' + port));
+//
